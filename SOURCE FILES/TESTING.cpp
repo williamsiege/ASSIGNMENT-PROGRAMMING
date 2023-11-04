67106d26da5f060115d1dc51a1969c5eda82d4c3
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-04 11:15:34 +0300

    ASSIGNMENT QUESTIONS

---
+++ SOURCE FILES/TESTING.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Hello world. this is myn first C++ program";
+    cout<<" I am very excited and ready to learn C++";
+    cout<<"This is a great University"; 
+    return 0;
+}
+
+// this is my helloworld program in C++
+//Created by William kagoiyo 
+//admission : 105241
+//Email: wkwabuiya@outlook.com
+
+/*hello world in 
+C++ Code*/
