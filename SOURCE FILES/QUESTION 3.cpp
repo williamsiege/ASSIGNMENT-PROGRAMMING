6b964828b83b999198f5925b6d0dfbc3f49f0e81
WILLSPHINX <wkwabuiya@outlook.com>
2023-11-05 15:14:13 +0300

    QNS

--- SOURCE FILES/QUESTION 3.cpp
+++ SOURCE FILES/QUESTION 3.cpp
@@ -25,24 +25,26 @@ cin >> score;
 // introducing variable grade to use in the switch case.
 
 char grade;
+string remark; // giving remarks on each grade.
+
 switch(score/10){
     case 8:
-       grade = 'A';
+       grade = 'A', remark = "Excellent WORK";
        break; //directs Compiler to stop execution and jump to the next case
     case 7:
-       grade = 'A';
+       grade = 'A', remark = "Good work but be above 80";
        break;
     case 6:
-       grade = 'B';
+       grade = 'B', remark = "Nice";
        break;
     case 5:
-       grade = 'C';
+       grade = 'C', remark = "Try Better";
        break;
     case 4:
-       grade = 'D';
+       grade = 'D', remark = "Try HARDER";
        break;
     case 3:
-       grade = 'F';
+       grade = 'F', remark = "Wake up";
        break;
     default:
     cout << "Invalid Score entered"<<endl;
@@ -54,6 +56,7 @@ cout << " Studnets's FullName: " << fullName<<endl;
 cout << "Course taken: " << course<<endl;
 cout << "Score: " << score<<endl;
 cout << "Grade: " << grade<<endl;
+cout << "Remark is :" <<remark<<endl;
 
 return 0;
 
